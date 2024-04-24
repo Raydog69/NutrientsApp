7737dcdcb6b45df8f512ff364432589cf965aecf
Raydog69 <trygve@garatun.no>
2024-04-24 13:43:00 +0200

    p

--- test.py
+++ test.py
@@ -1,3 +1,30 @@
-from datetime import datetime
-a = datetime.now().strftime('%Y-%m-%d')
-print(a)
+def a(n):
+    if n == 1:
+        return 2
+    else:
+        return a(n-1) + n  
+
+print(a(10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+def find(amount):
+    n = 1
+    sum = 0
+    while True:
+        sum += a(n)
+        if sum >= amount:
+            rest = sum - amount
+            return n-1, rest
+        n += 1
+
